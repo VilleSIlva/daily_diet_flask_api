0edcec2ee9e490dafd5d3c2b652c51f2c0188ee7
Ville Nunes <villenunescode@gmail.com>
2025-12-27 14:33:23 -0300

    rota de login do usuario

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask
 from database import db
 from model.User import User
 from model.Snack import Snack
+from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
+flask_login = LoginManager()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'your_secret_key'
+flask_login.login_view = 'login'
 
 db.init_app(app)
+flask_login.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
