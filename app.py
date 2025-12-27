080b0d5eab92cc3b8579da3de218d30bf6e599a2
Ville Nunes <villenunescode@gmail.com>
2025-12-27 17:29:22 -0300

    Rota para update da refeicao

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 from database import db
 from model.User import User
 from model.Snack import Snack
-from flask_login import LoginManager, login_required, login_user, current_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 from bcrypt import checkpw, hashpw
 from datetime import datetime
 
@@ -23,9 +23,12 @@ def load_user(user_id):
 
 @app.route('/login',methods=['POST'])
 def login():
-    data = request.get_json()
+    data = request.get_json(silent=True)
     email = data.get('email')
     password = data.get('password')
+
+    if not email or not password:
+        return jsonify({"message":"Missing required fields"}), 400
 
     user = User.query.filter_by(email=email).first()
 
@@ -56,12 +59,18 @@ def register():
     return jsonify({
         "message":"User created successful",
         "user": user.to_dict()
-    }), 200
+    }), 201
 
-@app.route("/snacks", methods=["POSt"])
+@app.route('/logout',methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message":"Logout success"})
+
+@app.route("/snacks", methods=["POST"])
 @login_required
 def create_snacks():
-    data = request.json
+    data = request.get_json()
 
     if not all([data.get('name'),data.get('description'), data.get('diet_date')]):
         return jsonify({"message":"Missing required fields"}), 400
@@ -82,14 +91,14 @@ def create_snacks():
     db.session.add(snack)
     db.session.commit()
 
-    return({
+    return jsonify({
         "message":"Snack created sucessful",
         "snack":snack.to_dict()
     })
 
 @app.route("/snacks",methods=["GET"])
 @login_required
-def get_tasks():
+def get_snacks():
     
     snacks = Snack.query.filter_by(user_id=current_user.id)
 
@@ -99,18 +108,51 @@ def get_tasks():
 
 @app.route("/snacks/<int:id>",methods=["GET"])
 @login_required
-def get_task(id):
+def get_snack(id):
     snack = Snack.query.get(id)
 
     if not snack:
         return jsonify({"message":"Snack not found"}), 404
 
-    if snack.id != current_user.id:
+    if snack.user_id != current_user.id:
         return jsonify({"message":"Unauthorized"}),403
 
     return jsonify({
-        "snacks":snack.to_dict()
+        "snack":snack.to_dict()
     })
+
+@app.route("/snacks/<int:id>", methods=['PUT'])
+def update_snack(id):
+    data = request.get_json()
+    snack = Snack.query.get(id)
+
+    if not snack:
+        return jsonify({"message":"Snack not found"}), 404
+
+    if snack.user_id != current_user.id:
+        return jsonify({"message":"Unauthorized"}),403
+
+    try:
+        diet_date = data.get('diet_date')
+        if diet_date:
+            diet_date = datetime.fromisoformat(data.get('diet_date'))
+        else:
+            diet_date = snack.diet_date
+    except ValueError:
+        return jsonify({"message":"Invalidad date"}), 400
+ 
+    snack.name = data.get('name',snack.name)
+    snack.description = data.get('description',snack.description)
+    snack.diet = data.get('diet',snack.diet)
+    snack.diet_date = diet_date
+
+    db.session.commit()
+
+    return jsonify({
+        "message":"Snack update successfully",
+        "snacks": snack.to_dict()
+    })
+
 
 if __name__ == "__main__":
     with app.app_context():
